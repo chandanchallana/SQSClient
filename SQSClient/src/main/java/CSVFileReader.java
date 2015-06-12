c79d59288574a7cc75a7abda254a46c794781aee
t_chalc <chandan.challana@autodesk.com>
2015-06-11 17:15:51 -0700

    Reformated the code

--- SQSClient/src/main/java/CSVFileReader.java
+++ SQSClient/src/main/java/CSVFileReader.java
@@ -14,73 +14,73 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 
-
 /**
  * @author Joker
- *
  */
 public class CSVFileReader {
 
-	private static final String[] FILE_HEADERS = {"X_EIDM_GUID","AGREE_NUM","ECA_ACCOUNT_CSN","CUSTOMER_ACCOUNT_NAME__C"};
-	private static final String GUID ="X_EIDM_GUID";
-	private static final String AGREE_NUMBER="AGREE_NUM";
-	private static final String ACCOUNT_CSN="ECA_ACCOUNT_CSN";
-	private static final String CUSTOMER_ACCOUNT_NAME="CUSTOMER_ACCOUNT_NAME__C";
+    private static final String[] FILE_HEADERS = {"X_EIDM_GUID", "AGREE_NUM", "ECA_ACCOUNT_CSN", "CUSTOMER_ACCOUNT_NAME__C"};
+    private static final String GUID = "X_EIDM_GUID";
+    private static final String AGREE_NUMBER = "AGREE_NUM";
+    private static final String ACCOUNT_CSN = "ECA_ACCOUNT_CSN";
+    private static final String CUSTOMER_ACCOUNT_NAME = "CUSTOMER_ACCOUNT_NAME__C";
 
 
-	public static List<String> readCsvFile(String fileName){
-		FileReader reader = null;
-		CSVParser csvFileParser = null;
-		List<String> SqsMessages = new ArrayList<String>();
-		CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(FILE_HEADERS);
-		ObjectMapper mapper = new ObjectMapper();
-		try{
+    public static List<String> readCsvFile(String fileName) {
+        FileReader reader = null;
+        CSVParser csvFileParser = null;
+        List<String> SqsMessages = new ArrayList<String>();
+        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(FILE_HEADERS);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
 
-			reader = new FileReader(fileName);
-			csvFileParser = new CSVParser(reader,csvFormat);
-			List csvRecords = csvFileParser.getRecords();
-			System.out.println("The size of csvRecords is "+csvRecords.size());
-			for(int i=1;i< csvRecords.size();i++){
-				CSVRecord record = (CSVRecord) csvRecords.get(i);
-				GuidMessage mssg = new GuidMessage();
-				mssg.setGuid(record.get(GUID));
-				mssg.setAggreementNumber(record.get(AGREE_NUMBER));
-				mssg.setAccountCsn(record.get(ACCOUNT_CSN));
-				mssg.setCustomerAccName(record.get(CUSTOMER_ACCOUNT_NAME));
-				try{
-					String str = mapper.writeValueAsString(mssg);
-					SqsMessages.add(str);
+            reader = new FileReader(fileName);
+            csvFileParser = new CSVParser(reader, csvFormat);
+            List csvRecords = csvFileParser.getRecords();
+            System.out.println("The size of csvRecords is " + csvRecords.size());
+            for (int i = 1; i < csvRecords.size(); i++) {
+                CSVRecord record = (CSVRecord) csvRecords.get(i);
+                GuidMessage mssg = new GuidMessage();
+                mssg.setGuid(record.get(GUID));
+                mssg.setAggreementNumber(record.get(AGREE_NUMBER));
+                mssg.setAccountCsn(record.get(ACCOUNT_CSN));
+                mssg.setCustomerAccName(record.get(CUSTOMER_ACCOUNT_NAME));
+                try {
+                    String str = mapper.writeValueAsString(mssg);
+                    SqsMessages.add(str);
 
-				}catch(Exception e){
-					e.printStackTrace();
-				}
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-			}
+            }
 
-			System.out.println("the size of the list is "+SqsMessages.size());
-			for(String mssg:SqsMessages){
+            System.out.println("the size of the list is " + SqsMessages.size());
+            for (String mssg : SqsMessages) {
 
-				System.out.println(mssg);
-			}
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-		}finally{
-			try{
-				reader.close();
-				csvFileParser.close();
-			}catch(IOException e){
-				e.printStackTrace();
-			}
+                System.out.println(mssg);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                reader.close();
+                csvFileParser.close();
+            } catch (JsonMappingException jme) {
+                jme.getMessage();
+            } catch (JsonGenerationException je) {
+                je.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-		}
-		return SqsMessages;
-	}
+        }
+        return SqsMessages;
+    }
 
-	public static void main(String args[]){
-		String fileName= "/Users/Joker/guids.csv";
-		//File file = new File("state_table.csv");
-		//System.out.println(System.getProperty("user.home"));
-
-		CSVFileReader.readCsvFile(fileName);
-	}
+    public static void main(String args[]) {
+        //TODO move the file to resources folder
+        String fileName = "/Users/Joker/guids.csv";
+        CSVFileReader.readCsvFile(fileName);
+    }
 }
